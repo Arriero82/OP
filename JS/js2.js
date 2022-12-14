444dee83efacb3a29d3d13e9deb0438bc4cef6c7
Arriero82 <gabriel.vulliud@gmail.com>
2022-12-14 10:13:15 -0300

    ejercicio 2 JS

---
+++ JS/js2.js
@@ -0,0 +1,11 @@
+const nombre = "Gabo";
+const edad = 40;
+const desarrollador =  false;
+const anio_nacimiento = new Date(1982, 7, 1, 20,00,00).toLocaleString()
+const favBook = {
+    titulo: "El ciclo de la puerta de la muerte",
+    autor: " Margaret Weis y Tracy Hickman",
+    anio: 1990
+}
+
+console.log(anio_nacimiento);
